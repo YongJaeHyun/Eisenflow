0a7ff015c43759bcc639df1a520db5d5dcab0878
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-08-23 15:34:44 +0900

    tailwindcss 설정

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lightgray: "#c6c9d1",
+        darkgray: "#798399",
+        linegray: "#d2d6e0",
+        yellow: "#FEC900",
+      },
+    },
+  },
+  plugins: [],
+};
