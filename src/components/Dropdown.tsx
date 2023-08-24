5c26ba1f85575a7feebbbfe6ee00654aa0ea6d16
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-08-24 23:26:34 +0900

    드롭다운 트리거 button 범위 축소, 데이터를 받아 보여줌

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 import useAnimation from "../hooks/useAnimation";
 
-const Dropdown = () => {
+interface IDropdown {
+  title: string;
+  content: string;
+}
+
+const Dropdown = ({ title, content }: IDropdown) => {
   const [isVisible, setIsVisible] = useState(false);
   const [handleTransitionEnd, triggerAnimation] = useAnimation(isVisible);
 
@@ -12,24 +17,26 @@ const Dropdown = () => {
 
   return (
     <>
-      <button type="button" className="flex justify-between" onClick={toggleIsVisible}>
-        <h3 className="font-semibold truncate">Send over wireframes</h3>
-        {isVisible ? (
-          <IoMdArrowDropup size={22} className="mt-0.5" />
-        ) : (
-          <IoMdArrowDropdown size={22} className="mt-0.5" />
-        )}
-      </button>
+      <div className="w-full flex justify-between items-center">
+        <h3 className="font-semibold break-keep">{title}</h3>
+        <button type="button" onClick={toggleIsVisible}>
+          {isVisible ? (
+            <IoMdArrowDropup size={20} className="mt-0.5" />
+          ) : (
+            <IoMdArrowDropdown size={20} className="mt-0.5" />
+          )}
+        </button>
+      </div>
       <div onTransitionEnd={handleTransitionEnd} className="overflow-hidden">
-        <div
-          className={`transition-all   ${
+        <p
+          className={`transition-all text-darkgray ${
             !triggerAnimation
-              ? "max-h-0 duration-[300ms] ease-out"
+              ? "max-h-0 duration-[200ms] ease-out"
               : `max-h-screen duration-[300ms] ease-in`
           }`}
         >
-          123ㅈㄷㄹㅈㅁㄹㄷㅈㄹㅈㅁㄹㄷㅈㅁㄹㅁㄷㅈㄹㄷㅈㄹㅎㅁㄷ죠ㅑㅕㄹㅎㅈ묘ㅕㅑㅎ래요ㅕㅑㅈㅎㄹ죠ㅕㅐㄹㅎㅈㅎ
-        </div>
+          {content}
+        </p>
       </div>
     </>
   );
