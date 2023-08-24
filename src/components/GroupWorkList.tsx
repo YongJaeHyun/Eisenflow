b8fa9e5f75d036845689c883bb1ca3424641dc99
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-08-24 23:28:17 +0900

    drag and drop 구현

--- src/components/GroupWorkList.tsx
+++ src/components/GroupWorkList.tsx
@@ -1,38 +1,62 @@
-import { useState } from "react";
 import { IconType } from "react-icons";
 import { BsPlus } from "react-icons/bs";
 import GroupWorkListCard from "./GroupWorkListCard";
+import { ICardData, currentAddGroupIdx, isModalVisible } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { Droppable } from "react-beautiful-dnd";
 
 interface IGroupWorkList {
   Icon: IconType;
   groupName: string;
+  groupIdx: string;
+  workList: ICardData[];
 }
 
-const GroupWorkList = ({ Icon, groupName }: IGroupWorkList) => {
-  const [workList, setWorkList] = useState([1, 2]);
+const GroupWorkList = ({ Icon, groupName, groupIdx, workList }: IGroupWorkList) => {
+  const setIsVisible = useSetRecoilState(isModalVisible);
+  const setCurrentAddIdx = useSetRecoilState(currentAddGroupIdx);
+
+  const showModal = () => {
+    setIsVisible(true);
+    setCurrentAddIdx(parseInt(groupIdx));
+  };
 
   return (
-    <div className="w-full">
-      <div className="flex px-5 py-3">
-        <Icon />
-        <h2 className="font-semibold">{groupName}</h2>
-      </div>
-      <div className="bg-white min-h-[8rem] shadow-lg relative">
-        <button
-          type="button"
-          className={`w-8 h-8 rounded-full flex justify-center items-center absolute z-20 ${
-            workList.length
-              ? "-top-4 -right-4"
-              : "top-[4rem] -translate-y-1/2 right-1/2 translate-x-1/2 "
-          } bg-yellow`}
+    <Droppable droppableId={groupIdx}>
+      {(provided) => (
+        <div
+          className="w-full lg:w-[22%]"
+          {...provided.droppableProps}
+          ref={provided.innerRef}
         >
-          <BsPlus size={24} className="text-white" />
-        </button>
-        {workList?.map((_, idx) => (
-          <GroupWorkListCard key={idx} />
-        ))}
-      </div>
-    </div>
+          <div className="flex items-center px-5 py-3">
+            <Icon />
+            <h2 className="font-semibold">{groupName}</h2>
+          </div>
+          <div className="bg-white shadow-lg relative">
+            <button
+              type="button"
+              className={`w-8 h-8 rounded-full flex justify-center items-center absolute z-20 ${
+                workList.length
+                  ? "-top-4 -right-4"
+                  : "top-[3rem] -translate-y-1/2 right-1/2 translate-x-1/2 "
+              } bg-yellow`}
+              onClick={showModal}
+            >
+              <BsPlus size={24} className="text-white" />
+            </button>
+            {workList.length ? (
+              workList.map((cardData, idx) => (
+                <GroupWorkListCard key={idx} cardData={cardData} idx={idx} />
+              ))
+            ) : (
+              <div className="w-full h-24 bg-white shadow-lg absolute"></div>
+            )}
+            {provided.placeholder}
+          </div>
+        </div>
+      )}
+    </Droppable>
   );
 };
 
--- src/components/GroupWorkListCard.tsx
+++ src/components/GroupWorkListCard.tsx
@@ -1,42 +1,46 @@
 import { useState } from "react";
-import { BsStarFill } from "react-icons/bs";
+import { ICardData } from "../atoms";
 import Dropdown from "./Dropdown";
+import Rate from "./Rate";
+import { Draggable } from "react-beautiful-dnd";
 
-const GroupWorkListCard = () => {
-  const [rate, setRate] = useState(0);
+interface IGroupWorkListCard {
+  cardData: ICardData;
+  idx: number;
+}
 
-  const changeRate = (rate: number) => setRate(rate);
+const GroupWorkListCard = ({
+  cardData: { deadline, rate, title, content },
+  idx,
+}: IGroupWorkListCard) => {
+  const [initRate, setRate] = useState(rate);
+  const month = deadline.getMonth() + 1;
+  const days = deadline.getDate();
 
   return (
-    <div className="flex border-b">
-      <div className="w-1/5 h-20 flex justify-center items-center">
-        <button type="button" className="w-4 h-4 rounded-full border-2"></button>
-      </div>
-      <div className="w-4/5 pr-4 my-5">
-        <div className="flex justify-between mb-1">
-          <span className="text-sm">~3월 2일</span>
-          <div className="flex items-center">
-            {Array(5)
-              .fill(null)
-              .map((_, idx) =>
-                rate >= idx ? (
-                  <button key={idx} onClick={() => changeRate(idx)}>
-                    <BsStarFill size={12} className={`text-yellow ${idx === 4 && "mr-1"}`} />
-                  </button>
-                ) : (
-                  <button key={idx} onClick={() => changeRate(idx)}>
-                    <BsStarFill
-                      size={12}
-                      className={`text-lightgray ${idx === 4 && "mr-1"}`}
-                    />
-                  </button>
-                )
-              )}
+    <Draggable draggableId={deadline.toDateString() + rate + title + content} index={idx}>
+      {(provided) => (
+        <div
+          className="flex border-b bg-white"
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <div className="w-1/5 h-24 flex justify-center items-center">
+            <button type="button" className="w-4 h-4 rounded-full border-2"></button>
+          </div>
+          <div className="w-4/5 pr-4 my-5">
+            <div className="flex justify-between mb-1">
+              <span className="text-sm">{`~${month}월 ${days}일`}</span>
+              <div className="flex items-center">
+                <Rate rate={initRate} setRate={setRate} size={12} />
+              </div>
+            </div>
+            <Dropdown title={title} content={content} />
           </div>
         </div>
-        <Dropdown />
-      </div>
-    </div>
+      )}
+    </Draggable>
   );
 };
 
