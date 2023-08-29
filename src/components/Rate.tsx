6cdd1b3a6529433f1f334abdc8e52041894cf80e
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-08-29 23:49:40 +0900

    카드로서 보여질 때는 중요도를 수정할 수 없도록 변경

--- src/components/Rate.tsx
+++ src/components/Rate.tsx
@@ -2,26 +2,36 @@ import { BsStarFill } from "react-icons/bs";
 
 interface IRate {
   rate: number;
-  setRate: React.Dispatch<React.SetStateAction<number>>;
+  setRate?: React.Dispatch<React.SetStateAction<number>>;
   size: number;
 }
 
 const Rate = ({ rate, setRate, size }: IRate) => {
-  const changeRate = (rate: number) => setRate(rate);
   return (
     <>
       {Array(5)
         .fill(null)
-        .map((_, idx) => (
-          <button type="button" key={idx} onClick={() => changeRate(idx)}>
-            <BsStarFill
-              size={size}
-              className={`${rate >= idx ? "text-yellow" : "text-lightgray"} ${
-                idx === 4 && "mr-1"
-              }`}
-            />
-          </button>
-        ))}
+        .map((_, idx) =>
+          setRate ? (
+            <button type="button" key={idx} onClick={() => setRate(idx)}>
+              <BsStarFill
+                size={size}
+                className={`${rate >= idx ? "text-yellow" : "text-lightgray"} ${
+                  idx === 4 && "mr-1"
+                }`}
+              />
+            </button>
+          ) : (
+            <p key={idx}>
+              <BsStarFill
+                size={size}
+                className={`${rate >= idx ? "text-yellow" : "text-lightgray"} ${
+                  idx === 4 && "mr-1"
+                }`}
+              />
+            </p>
+          )
+        )}
     </>
   );
 };
